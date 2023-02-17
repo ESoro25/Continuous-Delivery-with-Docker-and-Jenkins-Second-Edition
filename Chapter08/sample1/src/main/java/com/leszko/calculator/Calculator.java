de62c18652fe398cc0cac0b695e674ffd73942fa
Evan Soroken <esoroken@yahoo.com>
2023-02-17 21:55:56 +0000

    more breaks and fixes

--- Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
+++ Chapter08/sample1/src/main/java/com/leszko/calculator/Calculator.java
@@ -3,16 +3,10 @@ package com.leszko.calculator;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 
-/**
- * Define a calculator class to take two numbers and return the sum
-*/
 @Service
 public class Calculator {
         final static int umlNUMBER1 = 3;
 	@Cacheable("sum")
-/**
- * Function that takes two integer arguments and returns the sum
-*/
 	public int sum(int a, int b) {
 		return a + b;
 	}
